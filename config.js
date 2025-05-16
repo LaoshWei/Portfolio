23dfe0b3b53e9ba8b967985acbfc2b376acd58a5
LaoshWei <aqu04s06@gmail.com>
2025-05-16 15:41:57 +0800

    fix config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const RETRIEVE_LATEST_WORK_URL = `https://08e65860-4676-41d2-9995-6f546fd0df3f-00-b0003azewloq.sisko.replit.dev/latest-works`;
+API_URL = `https://08e65860-4676-41d2-9995-6f546fd0df3f-00-b0003azewloq.sisko.replit.dev`;
